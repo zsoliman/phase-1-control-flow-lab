ee20b280fe5d582504be9bc03a39e7385cdb8d6c
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-04 18:48:01 -0400

    all tests pass

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else { return 'No can do.' }
 }
 
-function ternaryCheckCity(){
+// let city;
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.'
+  }
+
 }
